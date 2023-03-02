db31397e4364c2137130c18132ae9669791ab49b
vaclavik-jakub <115144117+vaclavik-jakub@users.noreply.github.com>
2023-03-02 11:31:20 +0100

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,23 @@
+let calibrateX = 0
+let calibrateY = 0
+let calibrateZ = 0
+
+let accX = input.acceleration(Dimension.X)
+let accY = input.acceleration(Dimension.Y)
+let accZ = input.acceleration(Dimension.Z)
+
+
+basic.forever(function() {
+   
+    input.onButtonPressed(Button.A, function() {
+        ExpSense.restart()
+           
+           
+
+    })
+    
+
+
+
+    })
+})
